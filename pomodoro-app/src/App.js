1ee6edee5a1d782a280f50e1806520a30e299adc
mjc714 <chin.matthew1212@gmail.com>
2017-11-23 13:56:37 -0500

    Break Timer Select
    Added selection dropdown for selecting interval breaks.

--- pomodoro-app/src/App.js
+++ pomodoro-app/src/App.js
@@ -2,18 +2,96 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class BreakTime extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      breakTime: 3,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  // Handle change in dropdown menu.
+  handleChange(e){
+    // Update breakTime state value to
+    // what is selected in the dropdown.
+    this.setState({breakTime: e.target.value});
+  }
+
+  // Handle submit input.
+  handleSubmit(e){
+    alert("Submitted break time: " + this.state.breakTime);
+    e.preventDefault();
+  }
+
+  render(){
+    return(
+      <form className = "breakSelect" onSubmit={this.handleSubmit}>
+      <label className = "breakLabel">
+        Break time:
+        <select className="breakTime" value = {this.state.breakTime} onChange={this.handleChange}>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    );
+  }
+}
+
+class Start extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      time: 0,
+    };
+    this.startTimer = this.startTimer.bind(this);
+    this.endTimer = this.endTimer.bind(this);
+  }
+
+  // Start the timer.
+  startTimer(e){
+    alert("Hello World");
+  }
+  
+  // Signal to user that time as expired.
+  endTimer(e){
+    alert("Timer has Expired");
+  }
+  
+  render(){
+    return(
+    <button className = "startButton" onClick={() => this.startTimer()}>
+      Start
+    </button>
+    );
+  }
+}
+
+// Timer component that will render a timer
+// to count to 25 minutes and breaks(3-5 or 15-30).
 class Timer extends React.Component{
   constructor(props){
     super(props);
-    this.state ={
+    this.state = {
       time: 0,
-
-    }
+    };
+    this.tick = this.tick.bind(this);
   }
+
+  tick(){
+    this.setState(prevState =>({
+      time: prevState.time + 1
+    }));
+  }
+
   render(){
     return(
       <div>
-        <p>Hello World!</p>
+        Time: {this.state.time}
       </div>
     );
   }
@@ -21,35 +99,14 @@ class Timer extends React.Component{
 
 class App extends Component {
   render() {
-    // Start the timer.
-    function startTimer(e){
-      
-    }
-    // Signal to user that time as expired.
-    // function endTimer(e){
-    //   alert("Timer has Expired");
-    // }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Pomodoro Timer</h1>
         </header>
-        <form className = "breakSelect">
-          <label className = "breakLabel">
-            Break time:
-            <select className="breakTime">
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <button className = "startButton" onClick={() => startTimer()}>
-          Start
-        </button>
-        <br/>
+        <BreakTime></BreakTime> <br/>
+        <Start></Start>
         <Timer></Timer>
       </div>
     );
