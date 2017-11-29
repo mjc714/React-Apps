901b83613c9980e84e5879110c907e3dee4f5807
Matthew Chin <chin.matthew1212@gmail.com>
2017-11-29 15:15:28 -0500

    Deploy

--- pomodoro-app/src/App.js
+++ pomodoro-app/src/App.js
@@ -50,30 +50,9 @@ class EndBreakTimeInput extends React.Component {
   render() {
     const endBreakTime = this.props.endBreakTime;
     return (
-      <div className="breakTimes">
-        <form className="endBreakSelect">
-          <label className="endBreakLabel">
-            End Break Time:
-          </label>
-          <select className="endBreakTime" value={endBreakTime} onChange={this.handleEndChange}>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-          </select><br />
-        </form>
+      <div id="slidecontainer">
+        <input type="range" min="15" max="30" value={endBreakTime} className="slider" id="myRange" onChange={this.handleEndChange} />
+        <p>End Break Time: <span>{endBreakTime}</span></p>
       </div>
     );
   }
