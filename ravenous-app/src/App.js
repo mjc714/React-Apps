74e6278fedd4589271b5e578e4430426fef2c2fe
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-04 17:25:32 -0500

    Update with props.
    Update title of app.
    Removed redundant component use.
    Added props.

--- ravenous-app/src/App.js
+++ ravenous-app/src/App.js
@@ -4,13 +4,33 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 
+// Temporary business info.
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'Potato Pizzeria',
+  address: '1010 Potato Lane',
+  city: 'Potatoville',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [business,
+  business,
+  business,
+  business,
+  business,
+  business];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Reactvenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
