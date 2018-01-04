79b85a5415f7d45cbbf95129870ff4efd70267fe
Matthew Chin <chin.matthew1212@gmail.com>
2018-01-04 18:17:23 -0500

    Add UI feedback to SearchBar
    Added visual feedback for sorting options at the top of the search bar.
    
    Set the state of sorting options and input elements.
    
    Simulate search query with "Let's Go" button.

--- ravenous-app/src/App.js
+++ ravenous-app/src/App.js
@@ -25,11 +25,15 @@ const businesses = [business,
   business];
 
 class App extends Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Reactvenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
     );
